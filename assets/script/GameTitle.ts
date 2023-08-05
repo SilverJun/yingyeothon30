e2b121c6072c6746890554d99245123cff892213
SilverJun <jangej1031@gmail.com>
2023-08-05 15:32:22 +0900

    타이틀 수정

--- assets/script/GameTitle.ts
+++ assets/script/GameTitle.ts
@@ -1,8 +1,11 @@
-import { _decorator, Button, Component, director, Node } from 'cc';
+import { _decorator, Button, Component, director, Node, tween, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameTitle')
 export class GameTitle extends Component {
+
+    @property({type: Node})
+    bg: Node;
 
     @property({type: Button})
     btnStart: Button;
@@ -14,6 +17,13 @@ export class GameTitle extends Component {
     btnCredit: Button;
 
     start() {
+        tween(this.bg)
+            .repeatForever(tween()
+                .to(5, {position: new Vec3(-1005, 0, 0)})
+                .set({position: new Vec3(0, 0, 0)})
+            )
+            .start();
+
         if (this.btnStart) {
             this.btnStart.node.on(Button.EventType.CLICK, () => {
                 director.loadScene('GameScene');
